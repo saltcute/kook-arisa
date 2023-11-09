3164945c1e1edc8543531347439944eaf54e66f3
Hexona <xuanjiap@lolicon.ac.cn>
2023-11-09 11:05:16 -0500

    fix: initialization

--- src/init/client.ts
+++ src/init/client.ts
@@ -1,7 +1,8 @@
+import Kasumi from 'kasumi.js';
 import upath from 'upath';
 import CustomKasumi from './type';
 
 
-export const client: CustomKasumi = new CustomKasumi();
+export const client: CustomKasumi = new Kasumi() as CustomKasumi;
 
 client.config.loadConfigFile(upath.join(__dirname, '..', 'config', 'config.json'));
