8905932eab3a797ba63f4c10724c2d4e2888a8ab
Hexona <xuanjiap@lolicon.ac.cn>
2023-11-30 11:06:23 -0500

    fix: allow color in pm2

--- src/pm2/pm2-windows.js
+++ src/pm2/pm2-windows.js
@@ -1,10 +1,2 @@
-const { exec } = require('node:child_process');
-const process = exec("npm start", { windowsHide: true });
-
-process.stdout.on('data', function (data) {
-    console.log(data);
-});
-
-process.stderr.on('data', function (data) {
-    console.error(data);
-});
+const { spawn } = require('node:child_process');
+spawn("npm", ["start"], { windowsHide: true, stdio: 'inherit' });
