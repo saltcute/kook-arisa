e504e5b8236ac946bd6d3a4b4e3482ace977809a
saltcute <me@mizuno.ai>
2024-01-09 17:46:34 -0500

    feat: gracefully disconnect all streamer when kill

--- src/menu/arisaAdmin/command/kill.ts
+++ src/menu/arisaAdmin/command/kill.ts
@@ -1,10 +1,14 @@
 import { BaseCommand, BaseSession, CommandFunction } from "kasumi.js";
 import menu from "..";
+import { controller } from "menu/arisa";
 
 class AppCommand extends BaseCommand {
     name = 'kill';
     func: CommandFunction<BaseSession, any> = async (session) => {
         await session.send("Shutting down...");
+        for (const streamer of controller.activeStreamersArray) {
+            await streamer.disconnect("机器人被管理员关闭（重启），请稍后再试");
+        }
         process.exit(0);
     }
 }
