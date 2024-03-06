4f3a8b70caacdffce539e6cace6de3a31230acd4
saltcute <me@mizuno.ai>
2024-03-06 12:52:20 -0500

    feat: save playlist before killing

--- src/menu/arisaAdmin/command/kill.ts
+++ src/menu/arisaAdmin/command/kill.ts
@@ -1,4 +1,7 @@
+import delay from "delay";
 import { BaseCommand, BaseSession, CommandFunction } from "kasumi.js";
+import { controller } from "menu/arisa/index";
+import playlist from "menu/arisa/playback/lib/playlist";
 import menu from "..";
 import broadcast from "./message/broadcast";
 
@@ -6,8 +9,14 @@ class AppCommand extends BaseCommand {
     name = 'kill';
     description = '(font)Danger(font)[danger] (Somewhat) gracefully terminate the main process.';
     func: CommandFunction<BaseSession, any> = async (session) => {
-        await session.send("Shutting down...");
+        await session.send("Cleaning up...");
+        for (const streamer of controller.activeStreamersArray) {
+            playlist.user.save(streamer, streamer.INVITATION_AUTHOR_ID);
+        }
+        await delay(2000);
         await broadcast.broadcast("机器人被管理员关闭（重启），机器人重新启动后会自动重新连接语音。如果机器人上线后没有重连请手动重试。");
+        await session.send("Shutting down...");
+        await delay(300);
         process.exit(0);
     }
 }
