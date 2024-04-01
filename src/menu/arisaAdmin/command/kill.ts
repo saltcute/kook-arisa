a0957646ee9cf501801959aee3fbba2a0f2ae045
saltcute <me@mizuno.ai>
2024-03-31 22:42:59 -0400

    fix: wait for playlist to save

--- src/menu/arisaAdmin/command/kill.ts
+++ src/menu/arisaAdmin/command/kill.ts
@@ -11,9 +11,9 @@ class AppCommand extends BaseCommand {
     func: CommandFunction<BaseSession, any> = async (session) => {
         await session.send("Cleaning up...");
         for (const streamer of controller.activeStreamersArray) {
-            playlist.user.save(streamer, streamer.INVITATION_AUTHOR_ID);
+            await playlist.user.save(streamer, streamer.INVITATION_AUTHOR_ID);
         }
-        await delay(2000);
+        await delay(500);
         await broadcast.broadcast("机器人被管理员关闭（重启），机器人重新启动后会自动重新连接语音。如果机器人上线后没有重连请手动重试。");
         await session.send("Shutting down...");
         await delay(300);
